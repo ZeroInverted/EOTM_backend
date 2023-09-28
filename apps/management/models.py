a1fcb65133fe4f75ba2c1ebf0da47290d4db5264
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-09-28 22:33:53 +0300

    Add Employee model that inherits from AbstractUser, update settings

--- apps/management/models.py
+++ apps/management/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class Employee(AbstractUser):
+    job_title = models.CharField(max_length=128, null=True)
+    total_recommends = models.PositiveIntegerField(default=0)
+    current_month_recommends = models.PositiveIntegerField(default=0)
+    eotm_wins = models.PositiveIntegerField(default=0)
+    is_eotm = models.BooleanField(name="Employee of the month", default=False)
+    image = models.ImageField(null=True, blank=True)
+    image_url = models.CharField(max_length=200, null=True, blank=True)
+
+    def __str__(self) -> str:
+        return self.username
+
+    class Meta:
+        verbose_name = "Employee"
+        verbose_name_plural = "Employees"
+
+    # Functionality to assert that only one employee is EOTM:
+
+    def save(self, *args, **kwargs):
+
+        # unset all other EOTM flags if this employee has been chosen as EOTM:
+        if self.is_eotm:
+            Employee.objects.filter(is_eotm=True).exclude(
+                pk=self.pk).update(is_eotm=False)
+
+        super().save(*args, **kwargs)
--- eotm_backend/settings.py
+++ eotm_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "apps.management"
 ]
 
 MIDDLEWARE = [
@@ -129,3 +130,5 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+AUTH_USER_MODEL = "management.Employee"
