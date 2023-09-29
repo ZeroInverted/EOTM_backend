dd0a52f617fe98c2e3061296eb0debf65978b6a8
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-09-30 00:32:18 +0300

    Update registration settings for Employee to allow for password hashing

--- apps/management/admin.py
+++ apps/management/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from .models import Employee
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserChangeForm
 # Register your models here.
 
-admin.site.register(Employee)
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta(UserChangeForm.Meta):
+        model = Employee
+
+
+class CustomUserAdmin(UserAdmin):
+    form = CustomUserChangeForm
+
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {"fields": ("job_title", "total_recommends",
+         "current_month_recommends", "eotm_wins", "is_eotm", "image", "image_url")}),
+    )
+
+
+admin.site.register(Employee, CustomUserAdmin)
