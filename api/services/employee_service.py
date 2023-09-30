3edd7a68155156a8f6f5a4fe24de91a706843864
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-09-30 22:49:48 +0300

    Add service functions for FastAPI route handlers

---
+++ api/services/employee_service.py
@@ -0,0 +1,49 @@
+from schemas.response_schema import APIResponse
+from models.employee_model import SQLAlchemyEmployee
+from sqlalchemy.orm import Session
+from sqlalchemy import desc
+from sqlalchemy.exc import SQLAlchemyError
+from fastapi import HTTPException
+
+
+def get_eotm(db: Session) -> APIResponse:
+    try:
+        eotm = db.query(SQLAlchemyEmployee).filter(
+            SQLAlchemyEmployee.is_eotm == True)
+        if eotm:
+            return APIResponse(success=True, results=list(eotm), status_code=200)
+        else:
+            error = ["Employee of The Month not found"]
+            return APIResponse(success=False, messages=error, status_code=404)
+    except SQLAlchemyError as e:
+        error = [f"Error occurred while querying database: {str(e)}"]
+        return APIResponse(success=False, messages=error, status_code=500)
+    except HTTPException as e:
+        error = [f"HTTP exception has occurred: {str(e)}"]
+        return APIResponse(success=False, messages=error, status_code=403)
+    except Exception as e:
+        error = [f"An error has occurred: {str(e)}"]
+        return APIResponse(success=False, messages=error, status_code=500)
+
+
+def get_hall_of_fame(db: Session, type: str = "wins", no_limit: int = 5) -> APIResponse:
+    try:
+        if (type == "wins"):
+            hof = db.query(SQLAlchemyEmployee).order_by(
+                desc(SQLAlchemyEmployee.eotm_wins)).limit(no_limit)
+            return APIResponse(success=True, results=list(hof), status_code=200)
+        elif (type == "recommends"):
+            hof = db.query(SQLAlchemyEmployee).order_by(
+                desc(SQLAlchemyEmployee.total_recommends)).limit(no_limit)
+            return APIResponse(success=True, results=list(hof), status_code=200)
+        else:
+            error = ["Bad request, please check your query parameters."]
+            return APIResponse(success=False, messages=error, status_code=400)
+    except SQLAlchemyError as e:
+        error = [f"Error occurred while querying database: {str(e)}"]
+        return APIResponse(success=False, messages=error, status_code=500)
+    except HTTPException as e:
+        error = [f"HTTP exception has occurred: {str(e)}"]
+        return APIResponse(success=False, messages=error, status_code=403)
+    except Exception as e:
+        return APIResponse(success=False, messages=error, status_code=500)
