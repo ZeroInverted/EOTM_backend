a96008038cbe7b769c3ec180b43c7d709e32d951
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-09-30 20:00:12 +0300

    Add comments to clarify the registration of Employee

--- apps/management/admin.py
+++ apps/management/admin.py
@@ -4,15 +4,23 @@ from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm
 # Register your models here.
 
+# UserChangeForm is used by UserAdmin
+# UserAdmin is what Django uses to render the admin look for its user.
+# Since Employee extends AbstractUser it can also leverage UserAdmin if a new form that is based on UserChangeForm uses Employee as the model.
+
+# Create a new form that is based on UserChangeForm
+
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = Employee
 
+# Use the new form that is based on UserChangeForm
+
 
 class CustomUserAdmin(UserAdmin):
     form = CustomUserChangeForm
-
+    # Update the fields used by the formset that generates the form to include extra fields in Employee.
     fieldsets = UserAdmin.fieldsets + (
         (None, {"fields": ("job_title", "total_recommends",
          "current_month_recommends", "eotm_wins", "is_eotm", "image", "image_url")}),
