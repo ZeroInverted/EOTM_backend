eca3d37571afe8dd3bcb2705567ca904cc5f35c6
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-10-03 23:05:04 +0300

    Add inclusion for eotm detail's router in the fastapi app

--- api/fastapi_main.py
+++ api/fastapi_main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routers.employee_router import employee_router
+from routers.eotmdetail_router import eotmdetail_router
 from fastapi.middleware.cors import CORSMiddleware
 store = FastAPI()
 
 store.include_router(employee_router, prefix="/employee")
+store.include_router(eotmdetail_router, prefix="'eotmdetail")
 
 
 origins = ["*"]
