4931ee762865a0f35874705a903aa26c608f3894
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-10-04 22:23:11 +0300

    Add a collective pydantic response schema

--- api/schemas/response_schema.py
+++ api/schemas/response_schema.py
@@ -1,11 +1,18 @@
 from pydantic import BaseModel
 from .employee_schema import SAEmployee
 from .eotmdetail_schema import SAEOTMDetail
-from typing import Union
+from typing import Union, Generic
 
 
 class APIResponse(BaseModel):
     success: bool
-    results: Union[list(SAEmployee), list(SAEOTMDetail)] | None = None
+    results: Union[list[SAEmployee], list[SAEOTMDetail]] | None = None
+    messages: str | None = None
+    status_code: int = 200
+
+
+class CollectiveResponse(BaseModel):
+    success: bool
+    results: dict[str, list[Union[SAEmployee, SAEOTMDetail]]] | None = None
     messages: str | None = None
     status_code: int = 200
