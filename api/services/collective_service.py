ac848bcc35b5e5cb8904651e5273734a5f467f85
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-10-04 23:02:42 +0300

    Update collective service to propagate results of each service

--- api/services/collective_service.py
+++ api/services/collective_service.py
@@ -1,10 +1,5 @@
-from schemas.response_schema import CollectiveResponse, APIResponse
-from models.employee_model import SQLAlchemyEmployee
-from models.eotmdetail_model import SQLAlchemyEOTMDetail
+from schemas.response_schema import CollectiveResponse
 from sqlalchemy.orm import Session
-from sqlalchemy import desc
-from sqlalchemy.exc import SQLAlchemyError
-from fastapi import HTTPException
 from employee_service import get_eotm_data
 from eotmdetail_service import get_eotmdetail_data
 
@@ -13,11 +8,15 @@ def get_collective_data(db: Session) -> CollectiveResponse:
     try:
         eotm = get_eotm_data(db)
         comments = get_eotmdetail_data(db)
-        if not isinstance(eotm, APIResponse) and not isinstance(comments, APIResponse):
+        if eotm["success"] == True and comments["success"] == True:
             return CollectiveResponse(success=True, results={
                 "employee": eotm,
                 "comments": comments,
             })
+        elif eotm["success"] == False:
+            return CollectiveResponse(False, messages=eotm["messages"], status_code=eotm["status_code"])
+        elif comments["success"] == False:
+            return CollectiveResponse(False, messages=comments["messages"], status_code=comments["status_code"])
     except Exception as e:
         error = [f"An error has occurred {str(e)}"]
         return CollectiveResponse(success=False, messages=error, status_code=500)
