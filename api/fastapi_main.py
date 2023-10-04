a3fe9fb88d89444e47555823d3abb81f0053c1de
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-10-04 22:49:48 +0300

    Update FastAPI app name

--- api/fastapi_main.py
+++ api/fastapi_main.py
@@ -2,15 +2,15 @@ from fastapi import FastAPI
 from routers.employee_router import employee_router
 from routers.eotmdetail_router import eotmdetail_router
 from fastapi.middleware.cors import CORSMiddleware
-store = FastAPI()
+eotm = FastAPI()
 
-store.include_router(employee_router, prefix="/employee")
-store.include_router(eotmdetail_router, prefix="'/eotmdetail")
+eotm.include_router(employee_router, prefix="/employee")
+eotm.include_router(eotmdetail_router, prefix="'/eotmdetail")
 
 
 origins = ["*"]
 
-store.add_middleware(
+eotm.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
