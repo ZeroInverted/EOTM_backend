8d8f16e3898a67025b2af95cb7c3142528ec26fc
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-10-04 23:06:39 +0300

    Update fastapi main py file to include collective router

--- api/fastapi_main.py
+++ api/fastapi_main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from routers.employee_router import employee_router
 from routers.eotmdetail_router import eotmdetail_router
+from routers.collective_router import collective_router
 from fastapi.middleware.cors import CORSMiddleware
 eotm = FastAPI()
 
 eotm.include_router(employee_router, prefix="/employee")
 eotm.include_router(eotmdetail_router, prefix="/eotmdetail")
-
+eotm.include_router(collective_router, prefix="/collective")
 
 origins = ["*"]
 
