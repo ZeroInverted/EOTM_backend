1d16ea2e2699c2b74f487809231b0852622164e5
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-10-04 23:25:39 +0300

    Update a typo in both response models

--- api/schemas/response_schema.py
+++ api/schemas/response_schema.py
@@ -7,12 +7,12 @@ from typing import Union, Generic
 class APIResponse(BaseModel):
     success: bool
     results: Union[list[SAEmployee], list[SAEOTMDetail]] | None = None
-    messages: str | None = None
+    messages: list[str] | None = None
     status_code: int = 200
 
 
 class CollectiveResponse(BaseModel):
     success: bool
     results: dict[str, list[Union[SAEmployee, SAEOTMDetail]]] | None = None
-    messages: str | None = None
+    messages: list[str] | None = None
     status_code: int = 200
