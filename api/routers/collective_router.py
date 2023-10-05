d114d99dd512a574d2e8de02782ea1d7ec4f6b57
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-10-06 01:37:12 +0300

    Add collective APIs as requested by frontend dev

--- api/routers/collective_router.py
+++ api/routers/collective_router.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter, Depends
+from schemas.response_schema import GenericMultipleResponse
+from schemas.eotmdetail_schema import EOTMDetailInput, SAEOTMDetail
+from services.collective_service import get_collective_data, post_collective_comment
 from database import get_db
 from sqlalchemy.orm import Session
 
@@ -6,6 +9,10 @@ collective_router = APIRouter()
 
 
 @collective_router.get("/get_collective_data")
-def get_collective(db: Session = Depends(get_db)):
-    # TODO: implement service
-    pass
+def get_collective_eotm(db: Session = Depends(get_db)):
+    return get_collective_data(db)
+
+
+@collective_router.post("/post_collective_comment", response_model=GenericMultipleResponse[SAEOTMDetail])
+def create_collective_comment(eotm_detail: EOTMDetailInput, db: Session = Depends(get_db)):
+    return post_collective_comment(db, eotm_detail)
--- api/services/collective_service.py
+++ api/services/collective_service.py
@@ -1,21 +1,50 @@
 from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
+from fastapi import HTTPException
+from models.employee_model import SQLAlchemyEmployee
+from models.eotmdetail_model import SQLAlchemyEOTMDetail
+from schemas.response_schema import GenericMultipleObjects, GenericMultipleResponse, GenericSingleObject, GenericSingleResponse
+from schemas.eotmdetail_schema import EOTMDetailInput, SAEOTMDetail
 from .employee_service import get_eotm_data
 from .eotmdetail_service import get_eotmdetail_data
 
 
-# def get_collective_data(db: Session):
-#     try:
-#         eotm = get_eotm_data(db)
-#         comments = get_eotmdetail_data(db)
-#         if eotm["success"] == True and comments["success"] == True:
-#             return CollectiveResponse(success=True, results={
-#                 "employee": eotm,
-#                 "comments": comments,
-#             })
-#         elif eotm["success"] == False:
-#             return CollectiveResponse(False, messages=eotm["messages"], status_code=eotm["status_code"])
-#         elif comments["success"] == False:
-#             return CollectiveResponse(False, messages=comments["messages"], status_code=comments["status_code"])
-#     except Exception as e:
-#         error = [f"An error has occurred {str(e)}"]
-#         return CollectiveResponse(success=False, messages=error, status_code=500)
+# API architecture as per request of Daniel Habib[Frontend dev]
+
+def get_collective_data(db: Session):
+    try:
+        eotm = db.query(SQLAlchemyEmployee).filter(
+            SQLAlchemyEmployee.is_eotm == True).first()
+        comments = db.query(SQLAlchemyEOTMDetail).all()
+        collective_dict = {
+            "success": True,
+            "employee": eotm,
+            "comments": comments
+        }
+        return collective_dict
+    except Exception as e:
+        error = f"An error has occurred {str(e)}"
+
+        return error, 505
+
+
+# As per Daniel's request, when posting a comment, this service returns all comments.
+
+def post_collective_comment(db: Session, eotm_detail: EOTMDetailInput) -> GenericMultipleResponse[SAEOTMDetail]:
+    try:
+        new_eotm_detail = SQLAlchemyEOTMDetail(**(eotm_detail.model_dump()))
+        db.add(new_eotm_detail)
+        db.commit()
+        db.refresh(new_eotm_detail)
+        all_comments = db.query(SQLAlchemyEOTMDetail).all()
+        data = GenericMultipleObjects[SAEOTMDetail](objects=all_comments)
+        return GenericMultipleResponse[SAEOTMDetail](success=True, data=data)
+    except SQLAlchemyError as e:
+        error = [f"Error occurred while querying database: {str(e)}"]
+        return GenericMultipleResponse[SAEOTMDetail](success=False, messages=error, status_code=500)
+    except HTTPException as e:
+        error = [f"HTTP exception has occurred: {str(e)}"]
+        return GenericMultipleResponse[SAEOTMDetail](success=False, messages=error, status_code=403)
+    except Exception as e:
+        error = [f"An error has occurred: {str(e)}"]
+        return GenericMultipleResponse[SAEOTMDetail](success=False, messages=error, status_code=500)
