eb585dd49d6fb954e89130a84d6f2d369b2b812c
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-10-07 04:30:45 +0300

    Add a new django app to handle authentication

---
+++ apps/authentication/views.py
@@ -0,0 +1,44 @@
+from django.http import JsonResponse
+from django.views import View
+from django.views.decorators.csrf import csrf_exempt
+from django.middleware.csrf import get_token
+from django.contrib.auth import authenticate, login, logout
+from django.conf import settings
+import json
+import jwt
+
+class EmployeeLogin(View):
+    
+    def get(self, request):
+        csrf_token = get_token(request)
+        return JsonResponse({"X-CSRFToken": csrf_token})
+    
+    def post(self, request):
+        request_data = json.loads(request.body.decode("utf-8"))
+        username = request_data.get("username")
+        password = request_data.get("password")
+        user = authenticate(request=request, username=username, password=password)
+        if user:
+            login(request, user)
+            print(f"Logged in successfully! as {user.get_username()}")
+        else:
+            print("Login error")
+            return JsonResponse({
+                "success": False,
+                "messages": ["Login error"]
+            })
+        token = jwt.encode({"username": user.get_username()}, settings.SECRET_KEY)
+        return JsonResponse({"access_token": token})
+    
+class EmployeeLogout(View):
+    
+    def get(self, request):
+        csrf_token = get_token(request)
+        return JsonResponse({"X-CSRFToken": csrf_token})
+    
+    def post(self, request):
+        try:
+            logout(request)
+            return JsonResponse({"success":"True"})
+        except Exception as e:
+            return JsonResponse({"success":"False"})
--- eotm_backend/settings.py
+++ eotm_backend/settings.py
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "apps.management"
+    "apps.management",
+    "apps.authentication"
 ]
 
 MIDDLEWARE = [
