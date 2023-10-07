fb858fa63b9ba31b96fc104b9ddd88334ba7fbeb
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-10-07 21:19:46 +0300

    Add authentication router

---
+++ api/routers/auth_router.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter
+from services.auth_service import employee_login, employee_logout
+from schemas.auth_schema import LoginModel, LogoutAuthResponse, LoginAuthResponse
+
+auth_router = APIRouter()
+
+
+@auth_router.post("/employee_login", response_model=LoginAuthResponse)
+def auth_login(login_credentials: LoginModel):
+    return employee_login(login_credentials=login_credentials)
+
+@auth_router.post("/employee_logout", response_model=LogoutAuthResponse)
+def auth_login():
+    return employee_logout()
