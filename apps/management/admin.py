847cb5929ff36d6efd4838863726cf7886d42702
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-10-09 19:59:26 +0300

    Added confirmation to employee of the month selection

--- apps/management/admin.py
+++ apps/management/admin.py
@@ -1,8 +1,13 @@
+from typing import Any
 from django.contrib import admin
+from django.http.request import HttpRequest
 from .models import Employee
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.forms import UserChangeForm
-# Register your models here.
+from django.contrib import messages
+from admin_confirm import AdminConfirmMixin
+
+
 
 # UserChangeForm is used by UserAdmin
 # UserAdmin is what Django uses to render the admin look for its user.
@@ -18,13 +23,24 @@ class CustomUserChangeForm(UserChangeForm):
 # Use the new form that is based on UserChangeForm
 
 
-class CustomUserAdmin(UserAdmin):
+class CustomUserAdmin(AdminConfirmMixin, UserAdmin):
     form = CustomUserChangeForm
     # Update the fields used by the formset that generates the form to include extra fields in Employee.
     fieldsets = UserAdmin.fieldsets + (
         (None, {"fields": ("job_title", "total_recommends",
-         "current_month_recommends", "eotm_wins", "is_eotm", "image", "image_url")}),
+         "current_month_recommends", "eotm_wins", "is_eotm", "number_of_likes", "image", "image_url")}),
     )
+    # Use django-admin-confirm module to show confirmation page on changes
+    confirm_change = True
+    # On the following field that decides if the user should be employee of the month
+    confirmation_fields = ["is_eotm", ]
+    
+    #override save model to show a message if the user has been saved already, post save success message.
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        if obj.is_eotm:
+            messages.success(request, f"User '{obj}' has been set as Employee of The Month")
+        
 
-
+            
 admin.site.register(Employee, CustomUserAdmin)
--- eotm_backend/settings.py
+++ eotm_backend/settings.py
@@ -39,14 +39,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "apps.management",
+    "apps.authentication",
+    "admin_confirm",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
-    "apps.management",
-    "apps.authentication"
+    "django.contrib.staticfiles"
 ]
 
 MIDDLEWARE = [
