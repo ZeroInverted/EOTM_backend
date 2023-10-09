b722c98500621b4862823cd09d1e5b0ea22914bf
Mahmoud Fathy <58611801+ZeroInverted@users.noreply.github.com>
2023-10-09 04:51:57 +0300

    Added fastapi endpoints for password reset

--- api/routers/auth_router.py
+++ api/routers/auth_router.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from services.auth_service import employee_login, employee_logout
+from fastapi import APIRouter, Request
+from services.auth_service import employee_login, employee_logout, employee_password_reset
 from schemas.auth_schema import LoginModel, GeneralAuthResponse, LoginAuthResponse
 
 auth_router = APIRouter()
@@ -13,3 +13,6 @@ def auth_login(login_credentials: LoginModel):
 def auth_logout():
     return employee_logout()
 
+@auth_router.post("/employee_password_reset", response_model=GeneralAuthResponse)
+def auth_password_reset(password_reset_credentials: LoginModel, request: Request):
+    return employee_password_reset(password_reset_credentials=password_reset_credentials, request=request)
--- api/services/auth_service.py
+++ api/services/auth_service.py
@@ -1,5 +1,8 @@
 from schemas.auth_schema import LoginModel
+from fastapi import Request
 import requests
+import jwt
+
 
 SECRET_KEY = "django-insecure-@02$-!cc4n*!1!2ib4w&6*2!d^d6tv#664-ulc2-hx-^0g+xg1"
 
@@ -23,4 +26,23 @@ def employee_logout() -> dict:
     headers = {"X-CSRFToken": csrf_token}
     
     response = requests.post("http://localhost:7777/logout/", headers=headers, cookies=cookies)
+    return response.json()
+
+def employee_password_reset(password_reset_credentials: LoginModel, request: Request) -> dict:
+    access_token = request.headers.get("access_token")
+    if not access_token:
+        return {"success": False}.json()
+    else:
+        payload = jwt.decode(access_token, SECRET_KEY, algorithms=["HS256"])
+        user_id: str = payload.get("id")
+        if user_id is None:
+            return {"success": False}.json()
+    # Hit Django with GET to fetch a csrf token,
+    get_csrf = requests.get("http://localhost:7777/passwordreset/")
+    csrf_token = get_csrf.cookies.get("csrftoken")
+    # Use token as cookie and header
+    cookies = {"csrftoken": csrf_token}
+    headers = {"X-CSRFToken": csrf_token}
+    
+    response = requests.post("http://localhost:7777/passwordreset/", data=password_reset_credentials.model_dump_json(), headers=headers, cookies=cookies)
     return response.json()
